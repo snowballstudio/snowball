1f23e133e62f591a397d018fcbd5d9a21179c945
Michelle Zhao <michellezhao201304@gmail.com>
2026-07-20 05:04:33 +1000

    iOS HealthKit bridge registration

---
+++ ios/App/App/MainViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+import Capacitor
+
+class MainViewController: CAPBridgeViewController {
+    override open func capacitorDidLoad() {
+        bridge?.registerPluginInstance(DeviceDataPlugin())
+    }
+}
